b5209ffc1c724498e0b98b1fab4c62effe8e8b1e
vevis <vevisvs@gmail.com>
2023-09-22 16:24:15 -0300

    size for titles

--- src/components/container/ProductsContainer.jsx
+++ src/components/container/ProductsContainer.jsx
@@ -1,14 +1,32 @@
 import React from 'react'
+import { useState } from 'react'
 import ProductCard from '../productCard/ProductCard'
 import '../container/ProductsContainer.css'
 
 const ProductsContainer = () => {
+  const [sizeSelected, setSizeSelected] = useState("medium");
+
+  const fontSizeOptions = {
+    small: "0.8rem",
+    medium: "0.9rem",
+    big: "1rem",
+};
+
   return (
     <div className='main-container'>
         <span className='btn-container'>Total products in cart:
-           <span className='btn-total-products'>1</span> 
+           <p className='btn-total-products'>1</p> 
         </span>
-        <ProductCard />
+        <div className='select-list'>
+            <span>Select a font size for product titles</span>
+            <select className='list-sizes' defaultValue={1} onChange={(e) => setSizeSelected(e.target.value)}>
+              <option value={1} disabled></option>
+              <option value="small">Small</option>
+              <option value="medium">Medium</option>
+              <option value="big">Big</option>
+            </select>
+        </div>
+        <ProductCard sizeSelected={sizeSelected} fontSizeOptions={fontSizeOptions}/>
     </div>
   )
 }
--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -3,67 +3,74 @@ import data from '../data/data.json'
 import ButtonCard from '../button/ButtonCard';
 import { useState } from 'react';
 
-const ProductCard = () => {
+const ProductCard = ({sizeSelected, fontSizeOptions}) => {
     const [products, setProducts] = useState(data);
 
-    const handleEditClick = (product) => {
-        const updatedProducts = products.map((p) => ({
-            ...p,
-            isEditing: p.id === product.id,
-        }));
-
-        setProducts(updatedProducts);
-    }
-
-    const handleBlur = (product) => {
-        const updatedProducts = products.map((p) => ({
-            ...p,
-            isEditing: false,
-        }));
-
-        setProducts(updatedProducts);
-    }
-
-    const handleChangeTitle = (product, editedTitle) => {
-        const updatedProducts = products.map((p) => {
-            if (p.id === product.id) {
-                return { ...p, title: editedTitle };
+    const handleEditClick = (productId) => {
+        const updatedProducts = products.map((product) => {
+            if (product.id === productId) {
+                return { ...product, isEditing: true };
             }
-            return p;
+            return product;
         });
-
         setProducts(updatedProducts);
     }
 
-  return (
-    <div className='ctn-body'>
-        <section className='ctn-cards'>
-            {products.map((item, index) => {
-                return (
-                    <div key={index} className='card'>
-                        <img src={item.image}/>
-                        {item.isEditing ? (
-                            <input
-                                value={item.title}
-                                onChange={(e) => handleChangeTitle(item, e.target.value)}
-                                onBlur={() => handleBlur(item)}
-                            />
-                        ) : (
-                            <h3 className='card-title' onClick={() => handleEditClick(item)}>{item.title}</h3>
-                        )}
-                        <span className='card-price'>${item.price}.00</span>
-                        <span className='card-quantity'>{item.quantity}</span>
-                        <p className='card-description'>{item.description}</p>
-                        <div className='add-to-cart'>
-                            <ButtonCard /> 
+    const handleBlur = (productId) => {
+        const updatedProducts = products.map((product) => {
+            if (product.id === productId) {
+                return { ...product, isEditing: false };
+            }
+            return product;
+        });
+        setProducts(updatedProducts);
+    }
+
+    const handleChangeTitle = (productId, editedTitle) => {
+        const updatedProducts = products.map((product) => {
+            if (product.id === productId) {
+                return { ...product, title: editedTitle };
+            }
+            return product;
+        });
+        setProducts(updatedProducts);
+    }
+
+    return (
+        <div className='ctn-body'>
+            <section className='ctn-cards'>
+                {products.map((item, index) => {
+                    return (
+                        <div key={index} className='card'>
+                            <img src={item.image} alt={item.title} />
+                            {item.isEditing ? (
+                                <input className='edit-input'
+                                    value={item.title}
+                                    onChange={(e) => handleChangeTitle(item.id, e.target.value)}
+                                    onBlur={() => handleBlur(item.id)}
+                                />
+                            ) : (
+                                <h3
+                                    className='card-title'
+                                    style={{ fontSize: fontSizeOptions[sizeSelected] }}
+                                    onClick={() => handleEditClick(item.id)}
+                                >
+                                    {item.title}
+                                </h3>
+                            )}
+                            <span className='card-price'>${item.price}</span>
+                            <span className='card-quantity'>{item.quantity}</span>
+                            <p className='card-description'>{item.description}</p>
+                            <div className='add-to-cart'>
+                                <ButtonCard />
+                            </div>
+                            <p className='expanded-item'>Learn More</p>
                         </div>
-                        <p className='expanded-item'>Learn More</p>
-                    </div>
-                )
-            })}
-        </section>
-    </div>
-  )
+                    );
+                })}
+            </section>
+        </div>
+    );
 }
 
 export default ProductCard
