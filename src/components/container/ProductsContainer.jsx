27a660961c6ce156382830d84127a74afd8f430a
vevis <vevisvs@gmail.com>
2023-09-22 18:19:52 -0300

    responsive containers

--- src/components/container/ProductsContainer.jsx
+++ src/components/container/ProductsContainer.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState } from 'react'
 import ProductCard from '../productCard/ProductCard'
 import '../container/ProductsContainer.css'
+import data from '../data/data.json'
 
 const ProductsContainer = () => {
   const [sizeSelected, setSizeSelected] = useState("medium");
@@ -14,8 +15,14 @@ const ProductsContainer = () => {
 
   return (
     <div className='main-container'>
-        <span className='btn-container'>Total products in cart:
-           <p className='btn-total-products'>1</p> 
+        <span className='btn-container'>Total products:
+           <p className='btn-total-products'>
+              {
+                data.reduce((acc, item) => {
+                  return acc + item.quantity;
+                }, 0) 
+              }
+            </p> 
         </span>
         <div className='select-list'>
             <span>Select a font size for product titles</span>
